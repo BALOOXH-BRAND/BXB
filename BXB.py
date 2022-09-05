538fd9f08022b925bda8fc0532c406553d7efa22
BALOOCH <97346610+BALOOXH-BRAND@users.noreply.github.com>
2022-09-05 07:30:50 +0500

    Add files via upload

---
+++ BXB.py
@@ -0,0 +1,16 @@
+import os, platform
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('mkdir /sdcard/SSB')
+except:pass
+try:
+    import requests
+except:
+    os.system('pip install requests')
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import new
+
+elif bit == '32bit':
+    import new32
