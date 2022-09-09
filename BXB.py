8014d9c31f681e2484c72dbc94630f72b0ed71f6
BALOOCH <97346610+BALOOXH-BRAND@users.noreply.github.com>
2022-09-09 11:35:14 +0500

    Update BXB.py

--- BXB.py
+++ BXB.py
@@ -1,16 +1,2 @@
-import os, platform
-os.system('termux-setup-storage')
-os.system('git pull')
-try:os.system('mkdir /sdcard/SSB')
-except:pass
-try:
-    import requests
-except:
-    os.system('pip install requests')
-
-bit = platform.architecture()[0]
-if bit == '64bit':
-    import new
-
-elif bit == '32bit':
-    import new32
+from ali import buy
+buy()
