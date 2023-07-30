f763aeb3a2a732d200031f5c9522624fd090dcf4
BALOOCH <97346610+BALOOXH-BRAND@users.noreply.github.com>
2023-07-31 00:42:26 +0500

    Update BXB.py

--- BXB.py
+++ BXB.py
@@ -1,22 +1,22 @@
 #coding=utf-8
 import os, sys, platform
-os.system('https://facebook.com/groups/347577560720737/')
-os.system('rm -rf BXB.so BXB32.so')
+os.system('xdg-open https://facebook.com/groups/347577560720737/')
+os.system('rm -rf sad.so BXB32.so')
 
 try:
     if sys.argv[1]=='update':
-        os.system('rm -rf BXB.so BXB32.so')
+        os.system('rm -rf sad.so BXB32.so')
 except:
     pass
 
 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('BXB.so'):
-        os.system('curl -L https://github.com/BALOOXH-BRAND/BXB-PRO/blob/main/BXB.cpython-311.so?raw=true -o BXB.so') 
-        import BXB
+    if not os.path.isfile('sad.so'):
+        os.system('curl -L https://github.com/BALOOXH-BRAND/BXB-PRO/blob/main/sad.cpython-311.so?raw=true -o sad.so') 
+        import sad
     else:
-        import BXB
+        import sad
 
 elif bit == '32bit':
     if not os.path.isfile('BXB32.so'):
